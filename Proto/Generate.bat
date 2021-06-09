b7b3e31b3dff2415b7775cb90dbee5419db4c268
KCFindstr <792491320@qq.com>
2021-06-09 00:18:11 -0700

    Rename folder

---
+++ Proto/Generate.bat
@@ -0,0 +1,3 @@
+dotnet protogen --csharp_out=../Generated +names=original .\mHeader.proto
+dotnet protogen --csharp_out=../Generated +names=original .\cHeader.proto
+dotnet protogen --csharp_out=../Generated +names=original .\Chart.proto
