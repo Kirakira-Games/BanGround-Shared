18b9a448ac6bdbbe095bd8580730b34f4a87d5f2
KCFindstr <792491320@qq.com>
2021-08-01 00:37:32 -0700

    Update Generate.ps1

--- Proto/Generate.ps1
+++ Proto/Generate.ps1
@@ -1,5 +1,5 @@
 dotnet tool restore
 dotnet protogen --proto_path=.\ --csharp_out=..\Generated\CSharp\ +names=original **/*.proto
 ls ..\Generated\CSharp\*.cs | % {
-  (Get-Content $_.FullName).replace('    public enum ', '    [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]`n    public enum ') | Set-Content $_.FullName
+  (Get-Content $_.FullName).replace('    public enum ', '    [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]\n    public enum ') | Set-Content $_.FullName
 }
