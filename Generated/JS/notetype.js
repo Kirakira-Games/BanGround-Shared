0f9438dabab90a97b16cba9475135fa0d577b1ce
GitHub Action <411937015@qq.com>
2021-08-02 06:11:57 +0000

    Automated ProtoBuf Code Generation

---
+++ Generated/JS/notetype.js
@@ -0,0 +1,26 @@
+// source: Chart.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.NoteType');
+
+/**
+ * @enum {number}
+ */
+proto.NoteType = {
+  BPM: 0,
+  SINGLE: 1,
+  FLICK: 2,
+  SLIDETICK: 3,
+  SLIDETICKEND: 4
+};
+
