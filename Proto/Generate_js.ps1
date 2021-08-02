45d67c36978498d6b77acc2c068c8851f8c934d8
KCFindstr <792491320@qq.com>
2021-08-01 23:52:22 -0700

    Move generation script to Script\

--- Proto/Generate_js.ps1
+++
@@ -1,1 +0,0 @@
-..\protoc-3.17.3-win64\bin\protoc.exe --proto_path=.\ --js_out=import_style=commonjs,binary:..\Generated\JS\ *.proto
---
+++ Script/Generate_js.ps1
@@ -0,0 +1,1 @@
+..\protoc-3.17.3-win64\bin\protoc.exe --proto_path=..\Proto\ --js_out=import_style=commonjs,binary:..\Generated\JS\ ..\Proto\*.proto
